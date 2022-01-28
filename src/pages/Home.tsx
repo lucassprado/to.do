e229ff4659150fe244b444d32a93cdc38700046c
Lucas Prado <lucasntc2201@gmail.com>
2022-01-28 18:43:08 -0300

    Implemented to-do functions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, task]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => {
+      if (task.id == id) {
+        task.done = !task.done
+      }
+      return task
+    })
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
